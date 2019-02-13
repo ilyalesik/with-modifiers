8cb79159bc0609a42c05d6709891fb9dfc78227e
Ilia Lesik <ilialesik@gmail.com>
2019-02-13 12:48:11 +0300

    Initial commit 🎉

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// @flow
+import { applyStyleModifiers, styleModifierPropTypes } from "styled-components-modifiers";
+
+export type TComponentWithModifiers<Modifier> = React$ComponentType<{
+    modifiers?: Modifier | Array<Modifier | false> | false,
+    children?: React$Node
+}>;
+
+export default function withModifiers<T: { [string]: any }>(
+    config: T,
+): ((callback: (any) => any) => TComponentWithModifiers<$Keys<T>>) {
+    return callback => {
+        const result = callback(applyStyleModifiers(config));
+        if (process.env.NODE_ENV !== 'production') {
+            result.propTypes = {
+                modifiers: styleModifierPropTypes(config),
+            };
+            result.possibleModifiers = Object.keys(config);
+        }
+
+        return result;
+    };
+}
